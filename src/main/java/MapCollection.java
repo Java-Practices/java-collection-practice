f1199f261c60422e96c0f73f2412ab45ab5e6f1e
Eason Li <lijinghua@odc.cmbchina.cn>
2020-03-11 19:23:46 +0800

    Implement map and reduce test

--- src/main/java/MapCollection.java
+++ src/main/java/MapCollection.java
@@ -1,24 +1,21 @@
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MapCollection {
     
     public static List<Integer> doubleCollection(List<Integer> list) {
-        // // Need to be implemented
-        return null;
+        return list.stream().map(item -> item * 2).collect(Collectors.toList());
     }
     
     public static List<String> mapToStringCollection(List<Integer> list) {
-        // Need to be implemented
-        return null;
+        return list.stream().map(item -> Character.toString((char) (item + 96))).collect(Collectors.toList());
     }
     
     public static List<String> uppercaseCollection(List<String> list) {
-        // Need to be implemented
-        return null;
+        return list.stream().map(String::toUpperCase).collect(Collectors.toList());
     }
     
     public static List<Integer> transformTwoDimensionalToOne(List<List<Integer>> list) {
-        // Need to be implemented
-        return null;
+        return list.stream().flatMap(item -> item.stream()).collect(Collectors.toList());
     }
 }
--- src/main/java/ReduceCollection.java
+++ src/main/java/ReduceCollection.java
@@ -2,27 +2,22 @@ import java.util.List;
 
 public class ReduceCollection {
     public static int getMax(List<Integer> list) {
-        // Need to be implemented
-        return 0;
+        return list.stream().reduce(list.get(0), (a, b) -> a > b ? a : b);
     }
     
     public static double getAverage(List<Integer> list) {
-        // Need to be implemented
-        return 0;
+        return list.stream().mapToDouble(item -> item).average().orElse(0);
     }
     
     public static int getSum(List<Integer> list) {
-        // Need to be implemented
-        return 0;
+        return list.stream().reduce(0, (sum, item) -> sum + item);
     }
     
     public static double getMedian(List<Integer> list) {
-        // Need to be implemented
-        return 0;
+        return list.stream().mapToDouble(x -> x).sorted().skip(list.size() / 2 - (list.size() % 2 == 0 ? 1 : 0)).limit(list.size() % 2 == 0 ? 2 : 1).average().getAsDouble();
     }
     
     public static int getFirstEven(List<Integer> list) {
-        // Need to be implemented
-        return 0;
+        return list.stream().filter(item -> item % 2 == 0).findFirst().get();
     }
 }
